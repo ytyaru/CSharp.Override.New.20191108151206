dbc0c0ad511ad354be31020f7f4786662fd7cb9c
ytyaru <ytyaru@outlook.jp>
2019-11-08 15:25:31 +0900

    初回コミット

---
+++ src/OverrideNew/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OverrideNew
+{
+    class Base {
+        public virtual void V() => Console.WriteLine("V"); // virtual method
+        public void I() => Console.WriteLine("I"); // instance method
+    }
+    class Super : Base
+    {
+        public override void V() => Console.WriteLine("override!");
+        public new void I() => Console.WriteLine("new!");
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base b = new Base(); b.V(); b.I();
+            Console.WriteLine();
+
+            Super s = new Super(); s.V(); s.I();
+            Console.WriteLine();
+
+            Base bs = new Super(); bs.V(); bs.I();
+            Console.WriteLine();
+        }
+    }
+}
